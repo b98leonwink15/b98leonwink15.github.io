af949a25dd5c37c880dfa002ab8b2c6e237ef95c
b98_leonwink16 <161037259+b98leonwink15@users.noreply.github.com>
2025-10-22 00:40:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark');
+}
+
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('expanded');
+}
